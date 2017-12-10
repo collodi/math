9a4253a2784ca6d4aef2fd798de5fb1a93601f3f
geppetto <wkim149@gmail.com>
2017-12-10 00:37:05 -0500

    done

--- topics/asmd.js
+++ topics/asmd.js
@@ -1,10 +1,10 @@
 angular.module('problems', ['ngMaterial'])
-    .controller('prompt', ['$scope', function($scope) {
+    .controller('prompt', ['$scope', '$timeout', '$document', function($scope, $timeout, $document) {
 	var rand_num = function (min, max) {
 	    return Math.floor(Math.random() * (max - min + 1)) + min;
 	};
 
-	var lim = 30
+	var lim = 30;
 	var ops = ['+', '-', '\xd7', '\xf7'];
 	var get_prob = function () {
 	    var prob = [];
@@ -16,40 +16,49 @@ angular.module('problems', ['ngMaterial'])
 		var n = rand_num(-lim, lim);
 		if (n < 0)
 		    n = '(' + n + ')';
-		else if (n == 0 && prob[2 * i - 1] == '\xf7')
+		else if (n === 0 && prob[2 * i - 1] === '\xf7')
 		    n += rand_num(1, lim);
+
 		prob.splice(2 * i, 0, n);
 	    }
 	    return prob.join(' ');
+	};
+
+	var count = function (n, t) {
+	    $scope.timer = n;
+	    if (n === 0)
+		return;
+
+	    $timeout(count, t, true, n - 1, t);
+	};
+
+	$scope.check = function () {
+	    if ($scope.status === 2) {
+		$scope.go_next();
+		return;
+	    }
+
+	    count(2, 100);
+	    $scope.status = 1;
+	    if ($scope.answer === null)
+		return;
+
+	    var ans = eval($scope.answer.replace(' ', ''));
+	    var prob = eval($scope.problem.replace(/\xd7/g, '*').replace(/\xf7/g, '/'));
+	    if (Math.abs(ans - prob) < 0.0001)
+		$scope.status = 2;
 	};
 
 	$scope.go_next = function () {
 	    $scope.answer = null;
 	    $scope.problem = get_prob();
 	    $scope.status = 0; /* 0: first try, 1: incorrect, 2: solved */
+	    $scope.timer = 0;
 	};
 
-	$scope.check = function () {
-	    if ($scope.status == 2) {
-		go_next();
-		return;
-	    }
-
-	    $scope.status = 1;
-	    var ans = $scope.answer.replace(' ', '');
-	    if (['+', '*', '(', ')'].some(x => ans.includes(x))
-		|| (ans.includes('-') && ans[0] != '-')
-		|| ans.count('/') > 1)
-		return;
-
-	    ans = eval(ans);
-	    var prob = eval($scope.problem.replace('\xd7', '*').replace('\xf7', '/'));
-
-	    console.log('ans', ans);
-	    console.log('prob', prob);
-
-	    if (Math.abs(ans - prob) < 0.0001)
-		$scope.status = 2;
+	$scope.press = function (ev) {
+	    if (ev.which === 13)
+		$scope.check();
 	};
 
 	$scope.go_next();
