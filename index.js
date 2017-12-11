facf50d025484cdf88d438908b9aa151974102ae
geppetto <wkim149@gmail.com>
2017-12-10 22:30:15 -0500

    add mono * binomial menu

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ angular.module('topics', ['ngMaterial'])
     .controller('topiclist', ['$scope', function($scope) {
 	$scope.topics = [
 	    { title: '+, -, \xd7, \xf7', link: 'asmd' }
+	    { title: 'monomial \xd7 binomial', link: 'monobi' }
 	];
     }]);
