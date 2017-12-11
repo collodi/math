f09197c108531e35206d30ddfb49467d38641261
geppetto <wkim149@gmail.com>
2017-12-11 12:05:28 -0500

    generate problems

--- topics/monobi.js
+++ topics/monobi.js
@@ -1,6 +1,6 @@
-angular.module('problems', ['ngMaterial'])
+angular.module('problems', ['ngMaterial', 'ngSanitize'])
     .controller('prompt', ['$scope', '$timeout', function($scope, $timeout) {
-	var rand_num = function (min, max) {
+	const rand_num = function (min, max) {
 	    return Math.floor(Math.random() * (max - min + 1)) + min;
 	};
 
@@ -12,28 +12,69 @@ angular.module('problems', ['ngMaterial'])
 	    $timeout(count, t, true, n - 1, t);
 	};
 
-	var get_prob = function () {
+	const ops = ['+', '-'];
+	const vars = ['x', 'y', 'z', 'g', 'h'];
+	const get_term = function (pos = true) {
+	    const nvars = rand_num(1, 3);
+	    /* choose random number of vars */
+	    const shf_vars = vars.sort(_ => .5 - Math.random());
+	    const vs = shf_vars.slice(0, nvars);
+	    /* choose random exponents */
+	    const shf_exp = vs.map(_ => rand_num(1, 3));
+	    return { cf: (rand_num(0, 1) || pos ? 1 : -1) * rand_num(1, 10),
+		     variable: vs, exp: shf_exp };
+	};
+
+	const term_to_str = function (term) {
+	    var s = '';
+	    if (term.cf === -1)
+		s += '-';
+	    else if (term.cf !== 1)
+		s += term.cf;
+
+	    for (var i = 0; i < term.variable.length; i++) {
+		s += term.variable[i];
+		if (term.exp > 1)
+		    s += '<sup>' + term.exp[i] + '</sup>';
+	    }
+	    return s;
+	};
+
+	$scope.gen_prob = function () {
+	    $scope.prob = {};
+	    $scope.prob.monomial = get_term(false);
+	    $scope.prob.binomial = [get_term(false), get_term()];
+	    $scope.prob.op = ops[rand_num(0, 1)];
+
+	    $scope.problem = term_to_str($scope.prob.monomial);
+	    $scope.problem += ' (' + term_to_str($scope.prob.binomial[0]);
+	    $scope.problem += ' ' + $scope.prob.op + ' ';
+	    $scope.problem += term_to_str($scope.prob.binomial[1]) + ')';
 	};
 
 	$scope.check = function () {
 	    count(2, 100);
-	    $scope.status = 1;
-	    if ($scope.answer === null)
+	    if ($scope.answer === null) {
+		$scope.status = 1;
 		return;
+	    }
 
-	    if (/* right answer */) {
+	    /* check answer */
+	    if (false) {
 		if ($scope.status !== 2)
 		    $scope.status = 2;
 		else
 		    $scope.go_next();
+	    } else {
+		$scope.status = 1;
 	    }
 	};
 
 	$scope.go_next = function () {
 	    $scope.answer = null;
-	    $scope.problem = get_prob();
 	    $scope.status = 0; /* 0: first try, 1: incorrect, 2: solved */
 	    $scope.timer = 0;
+	    $scope.gen_prob();
 	};
 
 	$scope.press = function (ev) {
